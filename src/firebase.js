387da0c693afaabb552ade341f73e01df6e5f476
zeroHwan <990211fff@gmail.com>
2021-11-29 17:26:39 +0900

    1.1 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDbXQ5mUPESGkRd_uvJ4zAvMXLvx9b_84Y",
+    authDomain: "nwitter-338e3.firebaseapp.com",
+    projectId: "nwitter-338e3",
+    storageBucket: "nwitter-338e3.appspot.com",
+    messagingSenderId: "1077228303970",
+    appId: "1:1077228303970:web:8c6972dd733f543776c33b"
+  };
+
+ export default firebase.initializeApp(firebaseConfig);
